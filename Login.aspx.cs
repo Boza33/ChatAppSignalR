ba130b04f330d7b2c9231b7932ca0d9d65ba77f2
Matteooo13 <mpeuraca2921it@raf.rs>
2024-05-31 18:07:44 +0200

    Login, Users.cs

---
+++ Login.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChatApp
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        //Class Object  
+        ConnectionClass ConnC = new ConnectionClass();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSignIn_Click(object sender, EventArgs e)
+        {
+            string Query = "select * from tbl_Users where UserName='" + txtUserName.Value + "' and Password='" + txtPassword.Value + "'";
+            if (ConnC.IsExist(Query))
+            {
+                string UserName = ConnC.GetColumnVal(Query, "UserName");
+                Session["UserName"] = UserName;
+                Response.Redirect("Chat.aspx");
+            }
+            else
+                txtUserName.Value = "Invalid UserName or Password!!";
+        }
+    }
+}
