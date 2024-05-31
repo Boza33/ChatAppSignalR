7ced439acf5efd212961e5f011f95e3056d6cd94
Boza33 <mbozovic4521it@raf.rs>
2024-05-31 17:04:29 +0200

    Package manager, register.aspx, database
    Co-Authored-By: Mtopalovic02 <170311311+Mtopalovic02@users.noreply.github.com>

---
+++ Register.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChatApp
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        ConnectionClass ConnC = new ConnectionClass();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnRegister_ServerClick(object sender, EventArgs e)
+        {
+            string Query = "insert into tbl_Users(UserName,Password)Values('" + txtUserName.Value + "','" + txtPassword.Value + "')";
+            string ExistQ = "select * from tbl_Users where UserName='" + txtUserName.Value + "'";
+            if (!ConnC.IsExist(ExistQ))
+            {
+                if (ConnC.ExecuteQuery(Query))
+                {
+                    ScriptManager.RegisterStartupScript(this, GetType(), "Message", "alert('Congratulations!! You have successfully registered..');", true);
+                    Session["UserName"] = txtUserName.Value;
+                    Response.Redirect("Chat.aspx");
+                }
+            }
+            else
+            {
+                ScriptManager.RegisterStartupScript(this, GetType(), "Message", "alert('UserName is already Exists!! Please Try Different User name..');", true);
+            }
+        }
+    }
+
+}
